ecdb17a103302cc39374decae7a0fa19be94df74
RideOrDye <63518641+RideOrDye@users.noreply.github.com>
2020-10-28 22:56:18 -0700

    Add files via upload
    is Even exercise

---
+++ exercise21.js
@@ -0,0 +1,11 @@
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+  if (num % 2 == 0){
+      console.log('even')
+  }
+  else{
+      
+  }
+    //AND THIS LINE ↑↑↑↑↑
+}
+console.log(isEven)
