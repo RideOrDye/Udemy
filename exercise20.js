c42249af6980055873daeb7efa99632b3c1f4f6c
RideOrDye <63518641+RideOrDye@users.noreply.github.com>
2020-10-28 17:36:53 -0700

    Add files via upload
    string template literals

---
+++ exercise20.js
@@ -0,0 +1,6 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+const roll = `my roll is ${die1} and ${die2}, which equals to ${die1+die2}`
