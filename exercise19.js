07e4807a6fdcfc1f31818d0772f81185ff34bdef
RideOrDye <63518641+RideOrDye@users.noreply.github.com>
2020-10-28 17:02:21 -0700

    Add files via upload
    string method

---
+++ exercise19.js
@@ -0,0 +1,7 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard' //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+word.slice(5,10)
+let facialHair = 'board'.replace('o','e')
+console.log(facialHair)
